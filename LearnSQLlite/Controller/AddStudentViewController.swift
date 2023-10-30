985b962a8927775864189972ab3cb68f7d5eee23
Khakim <luthfiel99@gmail.com>
2023-10-30 23:19:28 +0700

    learn sqllite crud

---
+++ LearnSQLlite/Controller/AddStudentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddStudentViewController.swift
+//  LearnSQLlite
+//
+//  Created by Khakim on 30/10/23.
+//
+
+import UIKit
+
+class AddStudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LearnSQLlite/View/Cell/StudentCell.swift
@@ -0,0 +1,23 @@
+//
+//  StudentCell.swift
+//  LearnSQLlite
+//
+//  Created by Khakim on 30/10/23.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
